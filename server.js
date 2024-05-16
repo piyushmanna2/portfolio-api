32abebd043bf14b82a84f0215ca68b191dc1fc45
Piyush Manna <w3bpiyush@gmail.com>
2024-05-16 17:00:40 +0545

    commit

--- server.js
+++ server.js
@@ -7,40 +7,41 @@ const aboutRouter = require("./router/about-router");
 const techstackRouter = require("./router/techstack-router");
 const contactRouter = require("./router/contact-router");
 const cluster = require("node:cluster");
-const numCPUs = require("node:os").availableParallelism();
+const os = require("node:os");
+const numCPUs = os.cpus().length;
 const app = express();
 
 const corsOptions = {
   origin: "*",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,  
+  credentials: true, 
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
+const PORT = 4222;
+
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`);
-
 
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
     cluster.fork();
   });
 } else {
-  app.use("/app", projectRouter);
-  app.use("/app", aboutRouter);
-  app.use("/app", techstackRouter);
-  app.use("/app", contactRouter);
-
-  const PORT = 4222; 
   connectDb().then(() => {
+    app.use("/app", projectRouter);
+    app.use("/app", aboutRouter);
+    app.use("/app", techstackRouter);
+    app.use("/app", contactRouter);
+
     app.listen(PORT, () => {
-      console.log(`Listening on port http://localhost:${PORT}`);
+      console.log(`Worker ${process.pid} is listening on port http://localhost:${PORT}`);
     });
   }).catch(err => {
     console.error('Failed to connect to the database', err);
